5c43ec3dfc5e24b4a3555e06f36175c730b0e5b8
Prattay Sarkar <orginalprattaysarkar@gmail.com>
2023-07-08 15:34:36 +0530

    test1

--- webddoscli.py
+++ webddoscli.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
+import random
+
 from colorama import Fore, Style
-#bbvbgf
+
+#is is
+def fakeip():
+    ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+    return ip
+
 
 print(Fore.GREEN + "╔═════════╣ About ╠══════════╗" + Style.RESET_ALL)
 print(Fore.GREEN + "║+-     WebDDOScli V1.0    -+║" + Style.RESET_ALL)
@@ -13,4 +20,3 @@ print(Fore.RED + "║+-      FOR EDUCATION PURPOSES!     -+║" + Style.RESET_AL
 print(Fore.RED + "║+- Please, Don't MISUSE This Tool!  -+║" + Style.RESET_ALL)
 print(Fore.RED + "╚══════════════════════════════════════╝\n" + Style.RESET_ALL)
 print(Fore.BLUE + "|+- Trying To Connect To URL...")
-
